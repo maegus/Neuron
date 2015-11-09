f8a3bef3204ad10032a25d3986efb3a6499783d5
Draveness <stark.draven@gmail.com>
2015-11-09 12:07:50 +0800

    Added neuron demo

--- Example/Neuron/ViewController.swift
+++ Example/Neuron/ViewController.swift
@@ -7,17 +7,18 @@
 //
 
 import UIKit
+import Neuron
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let target: TargetObject = "view"
+        let signal: NeuronSignal = NeuronSignal(target: target)
+        view.neuron.basedOnTarget("view") { (value) -> () in
+            self.view.backgroundColor = UIColor.redColor()
+        }
+        signal.fire()
     }
 
 }
